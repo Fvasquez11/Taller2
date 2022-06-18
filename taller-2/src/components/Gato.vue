3be151c7af7d34b988cac0904b8ebc118d0875d9
balegria <balegria9@gmail.com>
2022-06-17 20:43:16 -0400

    listo

--- taller-2/src/components/Gato.vue
+++ taller-2/src/components/Gato.vue
@@ -4,7 +4,7 @@ import { useGatoStore } from "@/stores/GatoStore"
 export default {
   data() {
     return {
-
+     
     }
   },
   setup() {
@@ -21,97 +21,74 @@ export default {
     },
     reset: function () {
       this.gatoStore.reset()
-    }
+    },
   },
 };
 </script>
 
 <template>
   <main>
-    <div class="head">
-      <h1>Gato</h1>
-      <b-button @click="reset" variant="outline-dark" size="lg">Reset</b-button>
+    <div class="container d-flex flex-column mb-3 mt-5">
+      <h1 class="text-center">Gato</h1>
+      <b-button class="w-25 mx-auto" @click="reset" variant="outline-dark" size="lg">Reset</b-button>
+    </div>
+    <div class="container w-25 mb-2">
+      <b-card v-bind:title="this.gatoStore.title" class="text-center">
+        <b-form-input class="text-center" v-model="this.gatoStore.currentFigure" disabled></b-form-input>
+      </b-card>
     </div>
     <div class="container" style="width: 15%;">
       <div class="row" style="height: 10vh;">
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(0, 0, 0)" :disabled="gatoStore.disabled[0]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(0, 0, 0)" :disabled="gatoStore.disabled[0]"
+            v-bind:variant="gatoStore.matrixColor[0][0]" size="lg">
             {{ gatoStore.matrixState[0][0] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(0, 1, 1)" :disabled="gatoStore.disabled[1]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(0, 1, 1)" :disabled="gatoStore.disabled[1]"
+            v-bind:variant="gatoStore.matrixColor[0][1]" size="lg">
             {{ gatoStore.matrixState[0][1] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(0, 2, 2)" :disabled="gatoStore.disabled[2]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(0, 2, 2)" :disabled="gatoStore.disabled[2]"
+            v-bind:variant="gatoStore.matrixColor[0][2]" size="lg">
             {{ gatoStore.matrixState[0][2] }}</b-button>
         </div>
       </div>
       <div class="row" style="height: 10vh;">
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(1, 0, 3)" :disabled="gatoStore.disabled[3]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(1, 0, 3)" :disabled="gatoStore.disabled[3]"
+            v-bind:variant="gatoStore.matrixColor[1][0]" size="lg">
             {{ gatoStore.matrixState[1][0] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(1, 1, 4)" :disabled="gatoStore.disabled[4]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(1, 1, 4)" :disabled="gatoStore.disabled[4]"
+            v-bind:variant="gatoStore.matrixColor[1][1]" size="lg">
             {{ gatoStore.matrixState[1][1] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(1, 2, 5)" :disabled="gatoStore.disabled[5]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(1, 2, 5)" :disabled="gatoStore.disabled[5]"
+            v-bind:variant="gatoStore.matrixColor[1][2]" size="lg">
             {{ gatoStore.matrixState[1][2] }}</b-button>
         </div>
       </div>
       <div class="row" style="height: 10vh;">
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(2, 0, 6)" :disabled="gatoStore.disabled[6]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(2, 0, 6)" :disabled="gatoStore.disabled[6]"
+            v-bind:variant="gatoStore.matrixColor[2][0]" size="lg">
             {{ gatoStore.matrixState[2][0] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(2, 1, 7)" :disabled="gatoStore.disabled[7]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(2, 1, 7)" :disabled="gatoStore.disabled[7]"
+            v-bind:variant="gatoStore.matrixColor[2][1]" size="lg">
             {{ gatoStore.matrixState[2][1] }}</b-button>
         </div>
         <div class="col-4 p-0">
-          <b-button class="w-100 h-100" @click="changeState(2, 2, 8)" :disabled="gatoStore.disabled[8]" variant="outline-dark" size="lg">
+          <b-button class="w-100 h-100" @click="changeState(2, 2, 8)" :disabled="gatoStore.disabled[8]"
+            v-bind:variant="gatoStore.matrixColor[2][2]" size="lg">
             {{ gatoStore.matrixState[2][2] }}</b-button>
         </div>
       </div>
     </div>
   </main>
 </template>
-
-<style>
-.head {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 100px;
-  margin-top: 100px;
-  gap: 50px;
-  flex-flow: column wrap;
-}
-</style>
-
-<!-- <style>
-main {
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-}
-
-#container {
-  margin-top: 30%;
-  justify-content: space-evenly;
-  display: grid;
-  grid-template-columns: auto auto auto;
-}
-
-.button {
-  display: flex;
-  flex-grow: 1;
-}
-
-b-button {
-  border-bottom: cornflowerblue;
-}
-</style> -->
--- taller-2/src/stores/GatoStore.js
+++ taller-2/src/stores/GatoStore.js
@@ -10,7 +10,14 @@ export const useGatoStore = defineStore('useGatoStore', {
           ["", "", ""],
           ["", "", ""],
         ],
-      disabled: [false, false, false, false, false, false, false, false, false]
+      matrixColor:
+        [
+          ["outline-dark", "outline-dark", "outline-dark"],
+          ["outline-dark", "outline-dark", "outline-dark"],
+          ["outline-dark", "outline-dark", "outline-dark"],
+        ],
+      disabled: [false, false, false, false, false, false, false, false, false],
+      title: "Turno del jugador"
     }
   },
 
@@ -19,22 +26,38 @@ export const useGatoStore = defineStore('useGatoStore', {
     changeState(x, y) {
       this.matrixState[x][y] = this.currentFigure
       this.currentFigure === "O" ? this.currentFigure = "X" : this.currentFigure = "O"
+      if (this.checkWinner()) {
+        this.setWinner(this.checkWinner())
+        this.disabled = [true, true, true, true, true, true, true, true, true]
+        this.title = "Ha ganado el jugador:"
+      }
     },
     reset() {
       this.disabled = [false, false, false, false, false, false, false, false, false]
+      this.matrixColor = [
+        ["outline-dark", "outline-dark", "outline-dark"],
+        ["outline-dark", "outline-dark", "outline-dark"],
+        ["outline-dark", "outline-dark", "outline-dark"],
+      ]
       for (let i = 0; i < this.matrixState.length; i++) {
         for (let j = 0; j < this.matrixState[i].length; j++) {
           this.matrixState[i][j] = ""
         }
       }
       this.currentFigure = "X"
+      this.title = "Turno del jugador"
+    },
+    setWinner(positions) {
+      for (let i = 0; i < positions.length; i++) {
+        this.matrixColor[positions[i][0]][positions[i][1]] = "success"
+      }
     },
     checkWinner() {
       let counter = 0
       let currentStateTemp = ""
       let places = [["", ""],
-                    ["", ""],
-                    ["", ""]]
+      ["", ""],
+      ["", ""]]
       for (let x = 0; x < 3; x++) {
         counter = 0;
         currentStateTemp = this.matrixState[x][0];
@@ -87,7 +110,7 @@ export const useGatoStore = defineStore('useGatoStore', {
       for (let x = 0; x < 3; x++) {
         if (this.matrixState[x][2 - x] == currentStateTemp && this.matrixState[x][2 - x] != "") {
           places[x][0] = x
-          places[x][1] = 2-x
+          places[x][1] = 2 - x
           counter++
           if (counter == 3) {
             this.currentFigure === "O" ? this.currentFigure = "X" : this.currentFigure = "O"
@@ -95,7 +118,7 @@ export const useGatoStore = defineStore('useGatoStore', {
           }
         }
       }
-      return
+      return false
     }
   },
 })
